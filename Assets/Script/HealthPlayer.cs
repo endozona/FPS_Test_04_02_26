30358eeeac375603152d1f565538e06a0fe40f22
endozona <enzo072004@gmail.com>
2026-02-07 01:03:59 +0100

    Add files

--- Assets/Script/HealthPlayer.cs
+++ Assets/Script/HealthPlayer.cs
@@ -1,9 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthPlayer: MonoBehaviour
 {
-    [SerializeField] private float _currentHealth;
     
+     [SerializeField] private float _currentHealth = 30f;
+    private float maxHealth = 30f;
+    public Image healthBar;
+
+    private void Update()
+    {
+        healthBar.fillAmount = _currentHealth / maxHealth;
+    }
+
     public void PlayerDammage(int damage)
     {
         _currentHealth -= damage;
